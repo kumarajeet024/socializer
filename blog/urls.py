2f7547d72a6c10079da2eb2bfe4b15d9c41a80fc
kumarajeet024 <kumarajeet024@gmail.com>
2019-02-08 01:16:54 +0530

    post routing

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/',views.about, name='blog-about')
 ]
 #PostListView i.e. a class based view will look for template in form <app>/<models>_<viewtype>.html
