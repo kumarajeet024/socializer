cad767e52d9fa8fdc38e2499509c9c1a487f28a8
kumarajeet024 <kumarajeet024@gmail.com>
2019-02-11 18:08:23 +0530

    a

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Post
+from django.contrib.auth.models import User
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # Create your views here.
@@ -35,7 +36,7 @@ class PostListView(ListView):
      template_name = 'blog/home.html'
      context_object_name = 'posts'
      ordering = ['-date_posted']
-     paginate_by = 2
+     paginate_by = 5
 
 class PostDetailView(DetailView):
      model = Post
@@ -76,7 +77,15 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
              return True
          return False
 
+class UserPostListView(ListView):
+    model = Post
+    template_name = 'blog/user_posts.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    paginate_by = 5
 
+    def get_queryset(self):
+        user = get_object_or_404(User, username=self.kwargs.get('username'))
+        return Post.objects.filter(author=user).order_by('-date_posted')
 def about(request):
     #return HttpResponse('<h1>Blog About</h1>')
     return render(request, 'blog/about.html',{'title':'About'})
